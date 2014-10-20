7c78f440682f34cd176162c29ff6b532cc1a28ae
alfred sang <i5ting@126.com>
2014-10-20 23:33:12 +0800

    remove should npm

--- app/test/api_token.js
+++ app/test/api_token.js
@@ -1,6 +1,10 @@
 var request = require('supertest');
 var app = require('../app');
 
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+var should = require('chai').should();
+
 describe('GET /tokens', function(){
   it('respond with json', function(done){
     request(app)
@@ -9,6 +13,12 @@ describe('GET /tokens', function(){
       .expect(200)
       .end(function(err, res){
         if (err) return done(err);
+				
+				should.not.exist(err);
+				
+				
+				res.status.should.be.equal(200);
+				
         done()
       });
   })
