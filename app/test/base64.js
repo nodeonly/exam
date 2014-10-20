e767d907e619c56aae3ce70b7497e68c9202c74e
alfred sang <i5ting@126.com>
2014-10-20 23:02:18 +0800

    mocha test init ok

--- app/test/base64.js
+++ app/test/base64.js
@@ -2,7 +2,7 @@ var assert = require('chai').assert;
 var expect = require('chai').expect;
 require('chai').should();
 
-var Base64 = require('../lib/base64');
+var Base64 = require('../utils/lib/base64');
 
 
 describe('Base64', function(){
